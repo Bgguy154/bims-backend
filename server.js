b387b18c9584b708be2803e8b46871575000b29d
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2025-07-24 18:44:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
 // Routes
 const authRoutes = require("./routes/authRoutes");
 const bankRoutes = require("./routes/bankRoutes");
